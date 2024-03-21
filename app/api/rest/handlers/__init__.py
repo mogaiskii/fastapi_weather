2135099d12e235c1692865573dcc4b9b429b1fa0
mogaiskii <sani.mog@gmail.com>
2024-03-21 20:42:43 +0100

    autoload handlers

--- app/api/rest/handlers/__init__.py
+++ app/api/rest/handlers/__init__.py
@@ -1,1 +1,18 @@
-from .health_check import health_check
+import os
+import importlib
+from fastapi.routing import APIRoute
+
+
+__globals = globals()
+
+# autoload:
+for file in os.listdir(os.path.dirname(__file__)):
+    if file.startswith('__'):
+        continue
+    mod_name = file[:-3]   # strip .py at the end
+    mod = importlib.import_module('.' + mod_name, package=__name__)
+    items = dir(mod)
+    for item in items:
+        obj = getattr(mod, item)
+        if isinstance(obj, APIRoute):
+            __globals[mod_name] = obj
