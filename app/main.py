2147040ac4a616ffb554546fe9fbb0f9264a2d4e
mogaiskii <sani.mog@gmail.com>
2025-04-28 20:03:25 +0200

    CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 __all__ = ['settings', 'app']
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.rest.router import rest_app
 from app import app
 from settings import settings
 
 app.include_router(rest_app, prefix="/rest")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
